59c496c189bdfc36c8c45541fe5ebf1d52b152fa
Bartłomiej Czyżewski <bartlomiejczyzewski@outlook.com>
2021-11-13 12:17:03 +0100

    First commit

---
+++ trying.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import numpy as np
+
+
+data_path = r'C:\Users\bartl\OneDrive\Dokumenty\Visual Studio\Lets try streamlit\Inflation Data.csv'
+
+
+@st.cache
+def load_data():
+    df = pd.read_csv(data_path)
+    df['Value'].replace(':', np.nan, inplace=True)
+    df['Value'] = df['Value'].astype('float')
+    df['TIME'] = pd.to_datetime(df['TIME'])
+
+    return df
+
+
+data = load_data()
+
+
+df_chosen = data[data['GEO'] == 'Poland']
+
+
+line_chart = px.line(
+    df_chosen,
+    x='TIME', y='Value', title='HICP inflation rate in Poland',
+    labels={
+        "TIME": "Time",
+        "Value": "HICP inflation (annual rate of change)"
+    }
+)
+
+
+st.title('Inflation Watch')
+
+st.write(line_chart)
